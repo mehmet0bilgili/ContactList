40ab41361aec92bb1d44c2163582a8c50cd52936
mehmetbilgili <62013848+mehmetbilgili@users.noreply.github.com>
2021-07-19 00:13:55 +0300

    middle change

--- src/Person.java
+++ src/Person.java
@@ -2,12 +2,14 @@ import java.util.ArrayList;
 
 public class Person {
 
+    int id;
     String name;
     String surname;
     String telNumber;
     ArrayList<Person> contact;
 
-    public Person(String name, String surname, String telNumber) {
+    public Person(int id, String name, String surname, String telNumber) {
+        this.id = id;
         this.name = name;
         this.surname = surname;
         this.telNumber = telNumber;
@@ -18,8 +20,16 @@ public class Person {
         return name;
     }
 
+    public String getSurname() {
+        return surname;
+    }
+
     public String getTelNumber() {
         return telNumber;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public void addPerson(Person p) {
@@ -28,6 +38,37 @@ public class Person {
         } else {
             System.out.println("You are already add this, " + p.getName() + "/" + p.getTelNumber() + ", person.");
         }
+    }
+
+    public void deletePerson(int id) {
+        boolean gate = true;
+        for (Person p: this.contact) {
+            if (p.getId() == id) {
+                this.contact.remove(p);
+                gate = false;
+                break;
+            }
+        }
+        if (gate) System.out.println("There is no such user as id " + id);
+    }
+
+    public void listContact() {
+        for (Person p: this.contact)
+            System.out.println(p);
+    }
+
+    public void callSomeone(int id) {
+        for (Person p: this.contact) {
+            if (p.getId() == id) {
+                System.out.println(p + " is calling...");
+                break;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + "-" + this.getSurname() + "-" +this.getTelNumber();
     }
 
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,44 @@
+import java.util.Scanner;
+
 public class Tester {
     public static void main(String[] args) {
-        Person p1 = new Person("Ahmet", "Simsek", "05522458512");
-        Person p2 = new Person("Mehmet", "Bilgili", "05536768542");
+        Person p1 = new Person(1,"Mehmet", "Bilgili", "05536768542");
+        Person active;
+        System.out.println(p1);
+        int id = 2;
+
+        Scanner general = new Scanner(System.in);
+
+        active = p1;
+        boolean run = true;
+        while (run) {
+            System.out.println("ADD-DELETE-UPDATE-CALL-QUIT");
+            String command = general.nextLine();
+            switch (command) {
+                case "ADD":
+                    System.out.print("Enter name: ");
+                    String name = general.nextLine();
+                    System.out.println("\nEnter surname: ");
+                    String surname = general.nextLine();
+                    System.out.println("\nEnter telephone number: ");
+                    String tel = general.nextLine();
+                    active.addPerson(new Person(id, name, surname, tel));
+                    id++;
+                    break;
+                case "DELETE":
+                    active.listContact();
+                    System.out.println("Which user you want to delete. Enter id: ");
+                    int delId = general.nextInt();
+                    active.deletePerson(delId);
+                    break;
+                case "UPDATE":
+
+                    break;
+                case "QUIT":
+                    run = false;
+                    break;
+            }
+        }
 
     }
 }
